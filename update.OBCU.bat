e5d8ae5b8bd6dc1c5de555808d3514b55f4924d3
Alex-Alves-S <alexsandroalves321@hotmail.com>
2023-07-25 20:26:54 -0300

    upgrade no codigo

--- update.OBCU.bat
+++ update.OBCU.bat
@@ -11,13 +11,16 @@ echo %hora%
 
 echo            MENU DE TAREFAS
 echo ===========================================
-echo * 1. Comunicacao OBCU                      *
-echo * 2. Update All                            *
-echo * 3. Set Train Id                          *
-echo * 4. Get Train Id                          *
-echo * 5. Close                                 *
+echo * 1. Configuracao de IP                    *
+echo * 2. Comunicacao OBCU                      *
+echo * 3. Update All                            *
+echo * 4. Set Train Id                          *
+echo * 5. Get Train Id                          *
+echo * 6. Close                                 *
 echo ===========================================
-
+echo                    AVISO
+echo AO ESCOLHER A OPCAO 2 E 3, ESPERE A OBCU REINICIAR.
+echo ======================================================
 set /p opcao= Escolha uma opcao:
 echo ------------------------------------
 if %opcao% equ 1 goto opcao1
@@ -25,38 +28,50 @@ if %opcao% equ 2 goto opcao2
 if %opcao% equ 3 goto opcao3
 if %opcao% equ 4 goto opcao4
 if %opcao% equ 5 goto opcao5
+if %opcao% equ 6 goto opcao6
 
 :opcao1
 cls
 echo =============================
-echo *     COMUNICAO OBCU        *
+echo *     CONFIGURAÇÃO DE IP    *
 echo =============================
 netsh interface ip set address name="Ethernet" static 192.168.0.221 255.255.255.0 192.168.0.1
-ping 192.168.0.190
+echo Modificado o ip do computador para 192.168.0.221
 pause
 goto menu
 
 :opcao2
+cls
+echo =============================
+echo *     COMUNICAO OBCU        *
+echo =============================
+ping 192.168.0.190
+pause
+goto menu
+
+:opcao3
 cls
 echo =======================
 echo *      UPDATE-ALL     *
 echo =======================
 cd C:/OBS_LOCAL_UPDATE
 fab update-all
+echo ESPERE A OBCU REINICIAR
 pause
 goto menu
 
-:opcao3
+:opcao4
 cls
 echo ================================
 echo *        SET-TRAIN-ID          *
 echo ================================
 cd C:/OBS_LOCAL_UPDATE
 fab set-train-id
+echo ESPERE A OBCU REINICIAR
 pause
 goto menu
 
-:opcao4
+:opcao5
 cls
 echo ================================
 echo *        GET-TRAIN-ID          *
@@ -66,6 +81,6 @@ fab get-train-id
 pause
 goto menu
 
-:opcao5
+:opcao6
 cls
 exit
